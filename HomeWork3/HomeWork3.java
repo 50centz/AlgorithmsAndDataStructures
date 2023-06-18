def4fda79172d540bed68ace0f93e72690355885
Федоров Евгений Дом ПК <zen-g@mail.ru>
2023-06-18 13:10:39 +0300

    Edited third task

--- HomeWork3/HomeWork3.java
+++ HomeWork3/HomeWork3.java
@@ -11,77 +11,15 @@ public class HomeWork3 {
 }
 
 class List{
+
     static  Node head;
+    
     static class Node{
         int value;
         Node next;
     }
 
-    public static void pushFront(int value){
-        Node node = new Node();
-        node.value = value;
-        node.next = head;
-        head = node;
-    }
-
-    public static void popFront(){
-        if(head != null) {
-            head = head.next;
-        }
-    }
-
-    public static void print(){
-        Node cur = head;
-
-        while (cur != null){
-            System.out.printf("%d ", cur.value);
-            cur = cur.next;
-        }
-        System.out.println();
-    }
-
-    public static boolean contains(int value){
-        Node cur = head;
-        while (cur != null){
-            if(cur.value == value){
-                return true;
-            }
-            cur = cur.next;
-        }
-        return false;
-    }
-
-
-    public  static void pushBack(int value) {
-        Node node = new Node();
-        node.value = value;
-
-        if(head == null){
-            head = node;
-        }else{
-            Node cur = head;
-            while (cur.next != null){
-                cur = cur.next;
-
-            }
-            cur.next = node;
-        }
-    }
-
-    public  static void popBack(){
-        if(head != null){
-            if(head.next == null){
-                head = null;
-            }else{
-                Node cur = head;
-                while (cur.next.next != null){
-                    cur = cur.next;
-                }
-                cur.next = null;
-            }
-
-        }
-    }
+    
 
 
     public  static void reverse(){
